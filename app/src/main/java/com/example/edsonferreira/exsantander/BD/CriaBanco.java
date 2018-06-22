5264ae03c537fc732d5eb729c69935afc176542c
edson.ferreira <edferreira3005@gmail.om>
2018-06-22 13:37:37 -0300

    Criando processo de criação do Banco de dados, criando primeira tabela e chamando a criação no processo de busca dos dados do arquivo JSon

--- app/src/main/java/com/example/edsonferreira/exsantander/BD/CriaBanco.java
+++ app/src/main/java/com/example/edsonferreira/exsantander/BD/CriaBanco.java
@@ -1,4 +1,33 @@
 package com.example.edsonferreira.exsantander.BD;
 
-public class CriaBanco {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CriaBanco extends SQLiteOpenHelper {
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "ExSantander";
+    protected static final String CELLS_TABLE = "CELLS";
+    protected static final String FUND_TABLE = "FUND";
+
+    public static final String CREATE_CELLS = "create table if not exists "
+            + CELLS_TABLE + " ( _id integer primary key autoincrement, ID_CELL int, TYPE_CELL int" +
+            ",MESSAGE text, TYPEFIELD text, HIDDEN text,"
+            + "TOPSPACING double,SHOW int, REQUIRED text);";
+
+    public CriaBanco(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        //Executando comando para criar a tabela CELLS
+        sqLiteDatabase.execSQL(CREATE_CELLS);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
--- app/src/main/java/com/example/edsonferreira/exsantander/BuscaDados/Busca.java
+++ app/src/main/java/com/example/edsonferreira/exsantander/BuscaDados/Busca.java
@@ -1,21 +1,27 @@
 package com.example.edsonferreira.exsantander.BuscaDados;
 
+import android.content.Context;
 import android.content.res.Resources;
 
+import com.example.edsonferreira.exsantander.BD.CriaBanco;
 import com.example.edsonferreira.exsantander.BuscaDados.Conexao.Conexao;
 import com.example.edsonferreira.exsantander.R;
 
 public class Busca {
 
+    public void BuscInfo(Context tela) {
 
-    //Buscando dados da URL
+        //Criar Banco de dados antes de tudo.
+        new CriaBanco(tela);
 
-    Conexao con = new Conexao();
+        //Buscando dados da URL
+        Conexao con = new Conexao();
 
-    //Primeira URL de dados
-    String Dados1 = con.BuscaUrl(Resources.getSystem().getString(R.string.url1));
+        //Primeira URL de dados
+        String Dados1 = con.BuscaUrl(Resources.getSystem().getString(R.string.url1));
 
-    //Segunda URL de dados
-    String Dados2 = con.BuscaUrl(Resources.getSystem().getString(R.string.url2));
+        //Segunda URL de dados
+        String Dados2 = con.BuscaUrl(Resources.getSystem().getString(R.string.url2));
+    }
 
 }
