de14846ff08b322844074f7f7f033d556481ad38
edson.ferreira <edferreira3005@gmail.om>
2018-06-25 09:43:32 -0300

    Fim do desenvolvimento do app

--- app/src/test/java/com/example/edsonferreira/exsantander/TestesUnitarios.java
+++ app/src/test/java/com/example/edsonferreira/exsantander/TestesUnitarios.java
@@ -1,22 +1,55 @@
 package com.example.edsonferreira.exsantander;
 
+import android.Manifest;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.view.LayoutInflater;
+import android.widget.Button;
 
 import com.example.edsonferreira.exsantander.BuscaDados.Conexao.Conexao;
+import com.example.edsonferreira.exsantander.Telas.PrincipalActivity;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class TestesUnitarios {
+
+    PrincipalActivity mockTela = Mockito.mock(PrincipalActivity.class);
+    PrincipalActivity spyTela = Mockito.spy(PrincipalActivity.class);
+
     @Test
-    public void TestConection() {
+    public void TestBanco() {
 
-        Conexao con = new Conexao();
+        mockTela.checkSelfPermission(Manifest.permission.INTERNET);
 
-        String valor = con.BuscaUrl("https://floating-mountain-50292.herokuapp.com/cells.json");
+        mockTela.databaseList();
 
 
-        assertFalse(valor.isEmpty());
     }
+
+    @Test
+    public void url(){
+
+        Conexao mockconect = Mockito.mock(Conexao.class);
+
+        String result="";
+        result = mockconect.BuscaUrl("https://floating-mountain-50292.herokuapp.com/cells.json");
+
+
+    }
+    @Test
+    public void BtnEnviar(){
+
+
+        Button btnEnviar = Mockito.spy((Button) spyTela.findViewById(R.id.btnEnviar));
+
+        btnEnviar.performClick();
+
+
+
+    }
+
+
 }
